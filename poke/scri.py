8a68e1afb8f51cf59380d8c47ced22e23ea182be
c7c7c7c7 <c7c7.c7ll@gmail.com>
2019-01-25 21:41:25 +0900

    add

--- poke/scri.py
+++ poke/scri.py
@@ -22,20 +22,14 @@ def get_HTML(url):
 
 
 for ID in range(1,810):
-
 	X = (str(ID)).zfill(3)
 	html=get_HTML( 'https://www.pokemon.jp/zukan/detail/'+X+'.html' )
-
-
 	for a in html:
 		result = re.search(pat1,a)
-
 		if result:
 			A = re.findall(pat1,a)
 			TAG = re.findall(pat2,A[0])
-
 			print( TAG[0] )
-
 			with open(TAG[0], mode="wb") as f:
 				f.write( urllib.request.urlopen(URL+A[0]).read() )
 
